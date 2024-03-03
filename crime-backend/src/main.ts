380324a48263972d345879f81d197d4670be105f
pawinwat <pawinwat_wat@hotmail.com>
2024-03-03 16:58:48 +0700

    split insert node logic and andd response interceptor to backend

--- crime-backend/src/main.ts
+++ crime-backend/src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { ResponseInterceptor } from './response/response.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('graph-api');
+  app.useGlobalInterceptors(new ResponseInterceptor());
 
   await app.listen(8000);
 }
