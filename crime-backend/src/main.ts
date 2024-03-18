8b4aceee28c44e6d468d3720f8f613739d7ea401
pawinwat <pawinwat_wat@hotmail.com>
2024-03-18 09:40:55 +0700

    add core swap year in etl and add axios in front end

--- crime-backend/src/main.ts
+++ crime-backend/src/main.ts
@@ -5,7 +5,20 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('graph-api');
   app.useGlobalInterceptors(new ResponseInterceptor());
+  app.enableCors({
+    origin: (origin, callback) => {
+      // console.log('ORIGIN', origin);
+      if (!origin || [].indexOf(origin) !== -1) {
+        // console.log('allow');
+        callback(null, true);
+      } else {
+        // console.log('not allow');
+        callback(null, true);
 
+        // callback(new Error('Not allowed by CORS'));
+      }
+    },
+  });
   await app.listen(8000);
 }
 bootstrap();
